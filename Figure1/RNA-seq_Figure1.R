80f49f1bf23d719072d76ebdff5469b4162bb64f
minami-folder <78523772+minami-folder@users.noreply.github.com>
2021-07-01 13:54:13 +0900

    Update RNA-seq_Figure1.R

--- Figure1/RNA-seq_Figure1.R
+++ Figure1/RNA-seq_Figure1.R
@@ -59,56 +59,125 @@ kddeg <- as.character(kddeg)
 wt2 <- wt[which(rownames(wt) %in% wtdeg),]
 kd2 <- kd[which(rownames(kd) %in% kddeg),]
 
-#Cluster DEGs into 5 clusters
-d <- wt2
-d <- genescale(d,axis=1,method="Z")
-set.seed(3)
-f1 <- cmeans(d,5,2,verbose=FALSE,m=1.3,dist="euclidean",method="cmeans")
-q1 <- as.data.frame(f1[3])
-a1 <- rownames(q1)[which(f1$cluster=="1")]
-a2 <- rownames(q1)[which(f1$cluster=="2")]
-a3 <- rownames(q1)[which(f1$cluster=="3")]
-a4 <- rownames(q1)[which(f1$cluster=="4")]
-a5 <- rownames(q1)[which(f1$cluster=="5")]
-length(a1)
-length(a2)
-length(a3)
-length(a4)
-length(a5)
+#Draw line graphs of mean fold change in expression of 5 DEG clusters in Ctrl
+ERG1 <- read.table("../Data/Subcluster1_ERGs.txt",sep="\t",header= F)
+ERG2 <- read.table("../Data/Subcluster2_ERGs.txt",sep="\t",header= F)
+ERG <- c(as.character(ERG1[,1]),as.character(ERG2[,1]))
+ERG <- sort(ERG)
 
-erg <- wt2[which(rownames(wt2) %in% a1),]
-irg <- wt2[which(rownames(wt2) %in% a2),]
-drg <- wt2[which(rownames(wt2) %in% a4),]
+IRG1 <- read.table("../Data/Subcluster1_IRGs.txt",sep="\t",header= F)
+IRG2 <- read.table("../Data/Subcluster2_IRGs.txt",sep="\t",header= F)
+IRG <- c(as.character(IRG1[,1]),as.character(IRG2[,1]))
+IRG <- sort(IRG)
+IRG[54] <- c("NKX3-1")
 
-d <- kd2[-which(rownames(kd2) %in% wtdeg),]
-d <- genescale(d,axis=1,method="Z")
-set.seed(1)
-f1 <- cmeans(d,3,2,verbose=FALSE,m=1.3,dist="euclidean",method="cmeans")
-q1 <- as.data.frame(f1[3])
-b1 <- rownames(q1)[which(f1$cluster=="1")]
-b2 <- rownames(q1)[which(f1$cluster=="2")]
-b3 <- rownames(q1)[which(f1$cluster=="3")]
-length(b1)
-length(b2)
-length(b3)
+DRG1 <- read.table("../Data/Subcluster1_DRGs.txt",sep="\t",header= F)
+DRG2 <- read.table("../Data/Subcluster2_DRGs.txt",sep="\t",header= F)
+DRG <- c(as.character(DRG1[,1]),as.character(DRG2[,1]))
+DRG <- sort(DRG)
 
-#Subclustering of ERGs, IRGs and DRGs
-setwt <- wtstandardfc[which(rownames(wtstandardfc) %in% rownames(erg)),]
-setkd <- kdstandardfc[which(rownames(kdstandardfc) %in% rownames(erg)),]
-deg <- cbind(setwt[,1:6],setkd[,1:6]) #ERGs
-#deg <- cbind(setwt[,1:10],setkd[,1:10]) #IRGs
-#deg <- cbind(setwt[,1:12],setkd[,1:12]) #DRGs
-deg <- as.data.frame(deg)
-zscore <- genescale(deg,axis=1,method="Z")
-set.seed(2) #ERGs
-#set.seed(3) #IRGs
-#set.seed(1) #DRGs
-f1 <- cmeans(zscore,2,2,verbose=FALSE,m=1.3,dist="euclidean",method="cmeans")
-q1 <- as.data.frame(f1[3])
-w1 <- rownames(q1)[which(f1$cluster=="1")]
-w2 <- rownames(q1)[which(f1$cluster=="2")]
+Downregulated <- read.table("../Data/Downregulatedcluster_12022019.txt",sep="\t",header= T)
+Downregulated <- Downregulated[,2]
+Downregulated <- as.character(Downregulated)
+Downregulated[93] <- c("SEPT5")
 
-w1 <- read.table("siIkBa_Upregulated.txt",sep="\t",stringsAsFactors = FALSE)
+Others <- read.table("../Data/Otherscluster_12022019.txt",sep="\t",header= T)
+Others <- Others[,2]
+Others <- as.character(Others)
+
+Upregulated <- read.table("../Data/siIkBa_Upregulated.txt",sep="\t",header= T)
+Upregulated <- Upregulated[,1]
+Upregulated <- as.character(Upregulated)
+
+Downregulated <- read.table("../Data/siIkBa_Downregulated.txt",sep="\t",header= T)
+Downregulated <- Downregulated[,1]
+Downregulated <- as.character(Downregulated)
+
+Others <- read.table("../Data/siIkBa_Others.txt",sep="\t",header= T)
+Others <- Others[,1]
+Others <- as.character(Others)
+
+wt2 <- wt[which(rownames(wt) %in% DRG),]
+data <- wt2
+for(k in 1:dim(wt2)[2]){
+  for(i in 1:dim(wt2)[1]){
+    data[i,k] <- data[i,k]/wt2[i,1]
+  }  
+}
+wtstandardfc <- data
+
+kd2 <- kd[which(rownames(kd) %in% DRG),]
+data <- kd2
+for(k in 1:dim(kd2)[2]){
+  for(i in 1:dim(kd2)[1]){
+    data[i,k] <- data[i,k]/kd2[i,1]
+  }  
+}
+kdstandardfc <- data
+
+wilcox.exact(x=wtstandardfc[,1],y=kdstandardfc[,1],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,2],y=kdstandardfc[,2],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,3],y=kdstandardfc[,3],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,4],y=kdstandardfc[,4],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,5],y=kdstandardfc[,5],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,6],y=kdstandardfc[,6],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,7],y=kdstandardfc[,7],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,8],y=kdstandardfc[,8],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,9],y=kdstandardfc[,9],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,10],y=kdstandardfc[,10],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,11],y=kdstandardfc[,11],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,12],y=kdstandardfc[,12],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfc[,13],y=kdstandardfc[,13],paired=F,alternative="g")
+
+wtstandardfcmean <- apply(wtstandardfc,2,mean)
+kdstandardfcmean <- apply(kdstandardfc,2,mean)
+wtstandardfcmean <- as.data.frame(wtstandardfcmean)
+kdstandardfcmean <- as.data.frame(kdstandardfcmean)
+wtstandardfcmean <- t(wtstandardfcmean)
+kdstandardfcmean <- t(kdstandardfcmean)
+wtstandardfcmean <- as.data.frame(wtstandardfcmean)
+kdstandardfcmean <- as.data.frame(kdstandardfcmean)
+
+colnames(wtstandardfcmean) <- c("0","15","30","45","60","75","90","105","120","135","150","165","180")
+wtstandardfcmean$t <- seq(1,dim(wtstandardfcmean)[1],1)
+p <- colnames(wtstandardfcmean)
+temp <- reshape2::melt(wtstandardfcmean,
+                       id="t",
+                       measure=p 
+)
+temp <- temp[1:(dim(temp)[1]-1),]
+temp$group <- rep("siCtrl",dim(temp)[1])
+wtstandardfcmeanextract <- temp
+
+colnames(kdstandardfcmean) <- c("0","15","30","45","60","75","90","105","120","135","150","165","180")
+kdstandardfcmean$t <- seq(1,dim(kdstandardfcmean)[1],1)
+p <- colnames(kdstandardfcmean)
+temp <- reshape2::melt(kdstandardfcmean,
+                       id="t",
+                       measure=p 
+)
+temp <- temp[1:(dim(temp)[1]-1),]
+temp$group <- rep("siIkBa",dim(temp)[1])
+kdstandardfcmeanextract <- temp
+wtstandardfcmeanextract$value <- as.numeric(wtstandardfcmeanextract$value)
+kdstandardfcmeanextract$value <- as.numeric(kdstandardfcmeanextract$value)
+df <- rbind(wtstandardfcmeanextract,kdstandardfcmeanextract)
+df$value <- as.numeric(df$value)
+df$variable <- as.character(df$variable)
+
+g <- ggplot(df,aes(x=variable,y=value,group=group))+
+  theme_bw(base_size = 16)+
+  theme(panel.grid = element_blank(),legend.position = "true")+
+  labs(x="",y="",title="",colour="")+
+  scale_x_discrete(breaks=c("0","15","30","45","60","75","90","105","120","135","150","165","180"),labels=c("0","","","","","","90","","","","","","180"),limits=c("0","15","30","45","60","75","90","105","120","135","150","165","180"))+
+  scale_y_continuous(breaks=seq(0.5,3.5,by=0.5),limits=c(0.5,3.5))+
+  #scale_y_continuous(breaks=seq(0.5,2.5,by=0.5),limits=c(0.5,2.5))+
+  geom_line(lwd=3.0,aes(color=group))+
+  scale_colour_manual(values = c(siCtrl="magenta",siIkBa="seagreen"))+
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5,size=20,face="italic"),panel.background=element_rect(color="black", size=2, fill="white" ),panel.grid.minor.y = element_blank(),panel.grid.major.x = element_line(colour="grey", size =1.0),panel.grid.major.y = element_blank(),panel.grid.minor.x = element_blank(),legend.position = "none",axis.title.x = element_text(size=15,margin=margin(t = 25, r = 0, b = 0, l = 0)),axis.title.y = element_text(size=16,margin=margin(t = 0, r = 25, b = 0, l = 0)),axis.text.x = element_text(size=15),axis.text.y = element_text(size=15));g
+
+#Draw line graphs of mean fold change in expression of 3 TNF-induced clusters in Ctrl
 #w1 <- read.table("Subcluster1_ERGs.txt",sep="\t",stringsAsFactors = FALSE)
 #w2 <- read.table("Subcluster2_ERGs.txt",sep="\t",stringsAsFactors = FALSE)
 #w1 <- read.table("Subcluster1_IRGs.txt",sep="\t",stringsAsFactors = FALSE)
@@ -119,24 +188,28 @@ w1 <- w1[,1]
 w2 <- w2[,1]
 
 #Draw a heatmap of the fold change in expression
-data <- wt
-for(k in 1:dim(wt)[2]){
-  for(i in 1:dim(wt)[1]){
-    data[i,k] <- data[i,k]/wt[i,1]
+wt2 <- wt[which(rownames(wt) %in% wtdeg),]
+kd2 <- kd[which(rownames(kd) %in% wtdeg),]
+wt2 <- wt2[which(rownames(wt2) %in% w1),]
+data <- wt2
+for(k in 1:dim(wt2)[2]){
+  for(i in 1:dim(wt2)[1]){
+    data[i,k] <- data[i,k]/wt2[i,1]
   }  
 }
 wtstandardfc <- data
 
-data <- kd
-for(k in 1:dim(kd)[2]){
-  for(i in 1:dim(kd)[1]){
-    data[i,k] <- data[i,k]/kd[i,1]
+kd2 <- kd2[which(rownames(kd2) %in% w1),]
+data <- kd2
+for(k in 1:dim(kd2)[2]){
+  for(i in 1:dim(kd2)[1]){
+    data[i,k] <- data[i,k]/kd2[i,1]
   }  
 }
 kdstandardfc <- data
 
-cwt <- wtstandardfc[which(rownames(wtstandardfc) %in% b2),]
-ckd <- kdstandardfc[which(rownames(kdstandardfc) %in% b2),]
+cwt <- wtstandardfc
+ckd <- kdstandardfc
 df1 <- cbind(cwt,ckd)
 df1 <- as.data.frame(df1)
 df1 <- genescale(df1,axis=1,method="Z")
@@ -176,6 +249,20 @@ kdstandardfcmean <- t(kdstandardfcmean)
 wtstandardfcmean <- as.data.frame(wtstandardfcmean)
 kdstandardfcmean <- as.data.frame(kdstandardfcmean)
 
+wilcox.exact(x=wtstandardfcextract[,1],y=kdstandardfcextract[,1],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,2],y=kdstandardfcextract[,2],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,3],y=kdstandardfcextract[,3],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,4],y=kdstandardfcextract[,4],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,5],y=kdstandardfcextract[,5],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,6],y=kdstandardfcextract[,6],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,7],y=kdstandardfcextract[,7],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,8],y=kdstandardfcextract[,8],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,9],y=kdstandardfcextract[,9],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,10],y=kdstandardfcextract[,10],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,11],y=kdstandardfcextract[,11],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,12],y=kdstandardfcextract[,12],paired=F,alternative="g")
+wilcox.exact(x=wtstandardfcextract[,13],y=kdstandardfcextract[,13],paired=F,alternative="g")
+
 colnames(wtstandardfcmean) <- c("0","15","30","45","60","75","90","105","120","135","150","165","180")
 wtstandardfcmean$t <- seq(1,dim(wtstandardfcmean)[1],1)
 p <- colnames(wtstandardfcmean)
@@ -209,7 +296,6 @@ g <- ggplot(df,aes(x=variable,y=value,group=group))+
   labs(x="",y="",title="",colour="")+
   scale_x_discrete(breaks=c("0","15","30","45","60","75","90","105","120","135","150","165","180"),labels=c("0","","","","","","90","","","","","","180"),limits=c("0","15","30","45","60","75","90","105","120","135","150","165","180"))+
   scale_y_continuous(breaks=seq(0.5,3.5,by=0.5),limits=c(0.5,3.5))+
-  #scale_y_continuous(breaks=seq(0.9,1.5,by=0.2),limits=c(0.9,1.5))+  
   geom_line(lwd=3.0,aes(color=group))+
   scale_colour_manual(values = c(siCtrl="magenta",siIkBa="seagreen"))+
   theme_bw()+
