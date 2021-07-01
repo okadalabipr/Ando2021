08104260185e713cc29885d9563b91dde213b2ba
minami-folder <78523772+minami-folder@users.noreply.github.com>
2021-07-01 13:25:08 +0900

    Update Immunostaining_Figure1.R

--- Figure1/Immunostaining_Figure1.R
+++ Figure1/Immunostaining_Figure1.R
@@ -42,6 +42,13 @@ library(ggplot2)
     two$mean <- apply(two,1,mean)
     wtsignal$nfkb <- two$mean
     
+    fcwtsignal <- c()
+    first <- wtsignal$nfkb[1]
+    for(i in 1:13){
+      fcwtsignal[i] <- wtsignal$nfkb[i]/first
+    }
+    wtsignal$nfkb <- fcwtsignal
+
     count <- read.csv("../Data/160712_ikbakd.csv")
     count1=count[,2:12]
     count1 <- data.frame(count1)
@@ -84,18 +91,56 @@ library(ggplot2)
     two$mean <- apply(two,1,mean)
     kdsignal$nfkb <- two$mean
     
-    g <- ggplot(kdsignal,aes(x=time,y=nfkb,color=group,size=group))+
+    fckdsignal <- c()
+    first <- kdsignal$nfkb[1]
+    for(i in 1:13){
+      fckdsignal[i] <- kdsignal$nfkb[i]/first
+    }
+    kdsignal$nfkb <- fckdsignal
+
+    df <- rbind(wtsignal,kdsignal)
+    df <- as.data.frame(df)
+    
+    g <- ggplot(df,aes(x=time,y=nfkb,group=group))+
       theme_bw(base_size = 16)+
-      theme(panel.grid = element_blank())+
+      theme(panel.grid = element_blank(),legend.position = "true")+
       labs(x="",y="",title="",colour="")+
-      #geom_line(lwd=2.2,color="magenta")+
-      geom_line(lwd=2.2,color="seagreen")+
       scale_x_continuous(breaks=seq(0,180,15),labels=waiver(),limits=c(0,180))+
       scale_y_continuous(breaks=seq(0.25,0.5,0.05),labels=waiver(),limits=c(0.25,0.5))+
-      geom_point(aes(shape=group,color=group))+
-      scale_color_manual(values=c(none="seagreen",atac="black"))+
-      scale_shape_manual(values = c(none=20,atac=21))+
-      scale_size_manual(values=c(none=0.0,atac=10.0))+ 
-      theme(panel.border=element_rect(fill=NA,color="black", size=1.3),panel.grid.major.x = element_line(colour="grey", size =0.7),panel.grid.major.y = element_blank(),legend.position = "none",title = element_blank(),legend.text = element_blank(),axis.text.x = element_text(size=12),axis.text.y = element_text(size=12));g
-
+      #scale_y_continuous(breaks=seq(1.0,1.75,0.15),labels=waiver(),limits=c(1.0,1.75))+
+      geom_line(lwd=3.0,aes(color=group))+
+      scale_colour_manual(values = c(siCtrl="magenta",siIkBa="seagreen"))+
+      theme_bw()+
+      theme(plot.title = element_text(hjust = 0.5,size=20,face="italic"),panel.background=element_rect(color="black", size=1.9, fill="white" ),panel.grid.minor.y = element_blank(),panel.grid.major.x = element_line(colour="grey", size =1.0),panel.grid.major.y = element_blank(),panel.grid.minor.x = element_blank(),legend.position = "none",axis.title.x = element_text(size=15,margin=margin(t = 25, r = 0, b = 0, l = 0)),axis.title.y = element_text(size=16,margin=margin(t = 0, r = 25, b = 0, l = 0)),axis.text.x = element_text(size=15),axis.text.y = element_text(size=15));g
+    
+    df <- data.frame(time=seq(0,10800,900),mrna=wtsignal$nfkb)
+    df <- df[1:13,]                                         
+    fp <- pchipfun(df[,1], df[,2]) 
+    f <- fp(seq(0, 10800, by = 1))
+    inputwt <- f
+    df <- data.frame(time=seq(0,10800,1),mrna=f)
+    signal <- data.frame(time = df[,1],nfkb = df[,2])
+    sigimp <- approxfun(signal$t, signal$nfkb, rule = 2)
+    inputwt <- sigimp
+    nfkb_wt <- signal
+    
+    df <- data.frame(time=seq(0,10800,900),mrna=kdsignal$nfkb)
+    df <- df[1:13,]                                         
+    fp <- pchipfun(df[,1], df[,2]) 
+    f <- fp(seq(0, 10800, by = 1))
+    inputkd <- f
+    df <- data.frame(time=seq(0,10800,1),mrna=f)
+    signal <- data.frame(time = df[,1],nfkb = df[,2])
+    sigimp <- approxfun(signal$t, signal$nfkb, rule = 2)
+    inputkd <- sigimp
+    nfkb_kd <- signal
+    
+    df <- rbind(nfkb_wt,nfkb_kd)
+    df <- as.data.frame(df)
+    df <- rescale(df[,2], to=c(0,1))
+    df <- data.frame(time=c(nfkb_wt[,1],nfkb_kd[,1]),nfkb=df)
+    nfkb_wt <- df[1:10801,]
+    nfkb_kd <- df[10802:21602,]
+    
+    wilcox.exact(x=nfkb_wt$nfkb[1:4501],y=nfkb_kd$nfkb[1:4501],paired=F,alternative="g")
     
